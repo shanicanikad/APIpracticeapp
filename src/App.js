f6859b7dd28d6cc1a6097c98804ee999f85c41f3
SHANICA_DENIS <shanicanikad@gmail.com>
2021-05-05 22:37:04 -0400

    everything is working so far

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MakeUpPage from "./makeUpPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+let url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl";
+
+const options = {
+  method: "GET",
+  headers: {
+    Accept: "application.json",
+  }
+};
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
+    this.findMakeup();
+  }
+
+
+  findMakeup = () => {
+    const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl";
+    fetch(url, {
+      headers: {
+        Accept: "applcation/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => this.setState({ isLoaded: true, items: data }));
+    console.log("Where are my results", this.state.items)
+  }
+
+  render() {
+    var { isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>;
+    }
+    else {
+      return (
+        <div className="App">
+          <div className = "list">
+           <h1>CoverGirl Cosmetics</h1>
+
+            {items.map(item => (
+              <div className= "group" key= {item.id}>
+                    <p>Product Name: {item.name}</p>
+                    <p><img src={item.image_link}/></p>  
+                    <p>Price: {item.price}</p>
+                    <p>Description: {item.description}</p>
+              </div>
+            
+            )
+    )}
+          </div>
+          <footer className="name">Shanica Denis ©2021</footer>
+
+        </div>
+        
+      );
+    }
+
+  }
 }
 
 export default App;
---
+++ src/makeUpPage.js
@@ -0,0 +1,52 @@
+// import React from 'react';
+// import "./makeUpPage.css";
+
+// function Brand(props) {
+//     return (
+//         <div className="title">
+//             <h1>CoverGirl Cosmetics</h1>
+//             <div class="box1">
+//                 <p>{props.name}</p>
+//             </div>
+//         </div>
+//     );
+// }
+
+// export default Brand;
+
+
+
+
+// import React, { Component } from "react";
+// import "./makeUpPage.css";
+
+
+// class Brand extends Component {
+//     constructor(props) {
+//         super();
+//         this.state = {
+//             result: [],
+//         }
+//     }
+
+//     render() {
+//         console.log(this.props.data);
+
+//         return (
+//             <div className="header">
+//             <h1>CoverGirl Cosmetics</h1>
+
+//                 <div className="box1">
+//                      {this.props.data.length !== 0
+//                         ? this.props.data.map((item) => (
+//                             <p>{item.results.map(request_hash => (<div> {request_hash.image_url} {request_hash.title} {request_hash.synopsis} {request_hash.rated} </div>))}
+//                             </p>
+//                         ))
+//                         : null}
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
+
+// export default Brand;
